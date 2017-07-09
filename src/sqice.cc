3a2043eb8e469760a9926d05768dc9f6cedb17f9
kvzhao <kelispinor@gmail.com>
2017-07-09 18:50:29 +0800

    sqice lsversion

--- src/sqice.cc
+++ src/sqice.cc
@@ -32,7 +32,8 @@ SQIceGame::SQIceGame (INFO info) : sim_info(info) {
     agent_map.resize(N, 0);
     canvas_traj_map.resize(N, 0);
     canvas_spin_map.resize(N, 0);
-    eng_map.resize(N, 0);
+    energy_map.resize(N, 0.0);
+    defect_map.resize(N, 0.0);
     diff_map.resize(N, 0);
 
     std::cout << "[GAME] Square Ice Game is created.\n";
@@ -43,6 +44,51 @@ void SQIceGame::reset_maps() {
               sites_counter.end(), 0);
     std::fill(canvas_traj_map.begin(),
               canvas_traj_map.end(), 0);
+    std::fill(agent_map.begin(),
+              agent_map.end(), 0);
+    std::fill(canvas_spin_map.begin(),
+              canvas_spin_map.end(), 0);
+    std::fill(energy_map.begin(),
+              energy_map.end(), 0.0);
+    std::fill(defect_map.begin(),
+              defect_map.end(), 0.0);
+    traj_sites.clear();
+    traj_spins.clear();
+    traj_norepeat.clear();
+
+    steps_counter = 0;
+}
+
+void SQIceGame::update_state_to_config() {
+    int diff = _cal_config_difference();
+    vector<int> backup = ice_config.Ising;
+    ice_config.Ising = state_t;
+    state_0 = state_t;
+    state_tp1 = state_t;
+    // state_0 = state_t 
+    // ... Sanity check!
+    if (   _cal_defect_density_of_state(ice_config.Ising) == 0.0 \
+        && _cal_energy_of_state(ice_config.Ising) == -1.0) {
+        std::cout << "[GAME] Updated Succesfully!\n";
+        updated_counter++; 
+        accepted_looplength.push_back(diff);
+   } else {
+        std::cout << "[GAME] Ice Config is RUINED. Restore.\n";
+        ice_config.Ising = backup;
+        restore_config_to_state();
+   }
+    // reset maps
+}
+
+void SQIceGame::UpdateConfig() {
+    update_state_to_config();
+    //?
+}
+
+void SQIceGame::restore_config_to_state() {
+    state_t = ice_config.Ising;
+    state_tp1 = ice_config.Ising;
+    state_0 = ice_config.Ising;
 }
 
 void SQIceGame::InitModel() {
@@ -64,9 +110,10 @@ void SQIceGame::MCRun(int mcSteps) {
         model.MCstep(&ice_config, &latt);
     }
     tt.timer_end();
+
     std::cout << "[GAME] Monte Carlo runs " 
-              << mcSteps << " steps with "
-              << tt.timer_duration() << " seconds.\n"; 
+               << mcSteps << " steps with "
+               << tt.timer_duration() << " seconds.\n"; 
     
     // Check whether it is icestates
     double Etot = model.total_energy(&ice_config, &latt);
@@ -75,20 +122,36 @@ void SQIceGame::MCRun(int mcSteps) {
     // Get the ising variables
     state_0 = ice_config.Ising;
     state_t = ice_config.Ising;
+    state_tp1 = ice_config.Ising;
+    config_mean = _cal_mean(state_0);
+    config_stdev = _cal_stdev(state_0);
+
     std::cout << "[GAME] Average Energy E = " << _cal_energy_of_state(state_0) << "\n";
     std::cout << "[GAME] Defect Density D = " << _cal_defect_density_of_state(state_0) << "\n";
+    std::cout << "[GAME] Config mean = " << config_mean << " , and std = " << config_stdev << "\n";
+}
+
+int SQIceGame::Start(int init_site) {
+    set_agent_site(init_site);
+    return agent_site;
+}
+
+void SQIceGame::Reset() {
+    reset_maps();
+    init_agent_site = agent_site;
+    restore_config_to_state();
 }
 
 // member functions 
 vector<double> SQIceGame::Metropolis() {
     vector<double> rets(4);
-    bool is_accpet = false;
+    bool is_accept = false;
     double E0 = _cal_energy_of_state(state_0);
     double Et = _cal_energy_of_state(state_t);
     double dE = Et - E0;
     double dd = _cal_defect_density_of_state(state_t);
-    int diff_counts = _cal_config_diff();
-    double diff_ratio = diff_counts / N;
+    int diff_counts = _cal_config_difference();
+    double diff_ratio = diff_counts / double(N);
     if (dE == 0.0) {
         is_accept = true;
         rets[0] = 1.0;
@@ -101,12 +164,37 @@ vector<double> SQIceGame::Metropolis() {
     return rets;
 }
 
+void SQIceGame::flip_site(int site) {
+    // maybe we need cal some info when flipping
+    if(site >= 0 && site < N) {
+        state_t[site] *= -1;
+    }
+}
+
+void SQIceGame::flip_along_traj(const vector<int> &traj) {
+    // check traj is not empty
+    // check traj is cont. or not
+    // flip along traj on state_t
+    // done, return ?
+    if(!traj.empty()) {
+        for (auto const & site : traj) {
+            flip_site(site);
+            #ifdef DEBUG
+            std::cout << "Flip site " << site 
+                    << " and dE = " << _cal_energy_of_state(state_t) - _cal_energy_of_state(state_0)
+                    << ", dd = " << _cal_defect_density_of_state(state_t) << endl;
+            #endif
+        }
+    }
+}
+
 void SQIceGame::set_agent_site(int site) {
     if (site >= 0 && site < N) {
         agent_site = site;
         if (traj_sites.size() == 0) {
             init_agent_site = site;
             traj_sites.push_back(site);
+            traj_norepeat.push_back(site);
         }
     } else {
         std::cout << "[GAME] WORNING, Set Agent on Illegal Site!\n";
@@ -129,50 +217,115 @@ int SQIceGame::get_agent_spin() {
     return get_spin(agent_site);
 }
 
-void SQIceGame::Draw(int dir_idx) {
+vector<double> SQIceGame::Draw(int dir_idx) {
     int site = -1;
+    int curt_spin = get_agent_spin();
+    int next_spin = 0;
+    vector<double> rets(3);
     // move agent
     switch (dir_idx) {
         case RIGHT:
+            next_spin = get_spin(get_neighbor_site_by_direction(RIGHT));
             site = go(RIGHT);
         break;
         case DOWN:
+            next_spin = get_spin(get_neighbor_site_by_direction(DOWN));
             site = go(DOWN);
         break;
         case LEFT:
+            next_spin = get_spin(get_neighbor_site_by_direction(LEFT));
             site = go(LEFT);
         break;
         case UP:
+            next_spin = get_spin(get_neighbor_site_by_direction(UP));
             site = go(UP);
         break;
         case UPPER_RIGHT:
+            next_spin = get_spin(get_neighbor_site_by_direction(UPPER_RIGHT));
             site = go(UPPER_RIGHT);
         break;
         case LOWER_RIGHT:
+            next_spin = get_spin(get_neighbor_site_by_direction(LOWER_RIGHT));
             site = go(LOWER_RIGHT);
         break;
         case LOWER_LEFT:
+            next_spin = get_spin(get_neighbor_site_by_direction(LOWER_LEFT));
             site = go(LOWER_LEFT);
         break;
         case UPPER_LEFT:
+            next_spin = get_spin(get_neighbor_site_by_direction(UPPER_LEFT));
             site = go(UPPER_LEFT);
         break;
         case UPPER_NEXT:
+            next_spin = get_spin(get_neighbor_site_by_direction(UPPER_NEXT));
             site = go(UPPER_NEXT);
         break;
         case LOWER_NEXT:
+            next_spin = get_spin(get_neighbor_site_by_direction(LOWER_NEXT));
             site = go(LOWER_NEXT);
         break;
     }
     // draw on canvas
-    canvas_traj_map[site] = 1;
+    if (canvas_traj_map[site] == 1) {
+        canvas_traj_map[site] = 0;
+    } else {
+        canvas_traj_map[site] = 1;
+    }
     canvas_spin_map[site] = state_t[site];
+
+    double dE = _cal_energy_of_state(state_tp1) - _cal_energy_of_state(state_t);
+    double dd = _cal_defect_density_of_state(state_tp1);
+
+    if (curt_spin == next_spin ) {
+        rets[0] = -1.0;
+    } else {
+        rets[0] = +1.0;
+    }
+    rets[1] = dE; rets[2] = dd;
+
+    #ifdef DEBUG 
+    std::cout << "  current spin " << curt_spin << " , next spin " << next_spin << "\n";
+    std::cout << " Draw dE = " << dE
+                << ", dd = " << dd << "\n";
+    #endif
+
+    return rets;
+}
+
+void SQIceGame::IceMove(int act_idx) {
+    int dir_idx; 
+    int agent_spin = get_agent_spin();
+
+    switch (act_idx) {
+        // go spin up
+        case 0:
+            if (agent_spin > 0) {
+                dir_idx = how_to_go(icemove(true));
+            } else {
+                dir_idx = how_to_go(icemove(false));
+            }
+            Draw(dir_idx);
+        break;
+        // go spin down
+        case 1:
+            if (agent_spin < 0) {
+                dir_idx = how_to_go(icemove(true));
+            } else {
+                dir_idx = how_to_go(icemove(false));
+            }
+            Draw(dir_idx);
+        break;
+    }
 }
 
 int SQIceGame::go(DIR dir) {
     int old_site = agent_site;
     int new_site = get_neighbor_site_by_direction(dir);
     set_agent_site(new_site);
+    if (!_is_visited(new_site)) {
+        traj_norepeat.push_back(new_site);
+    }
+    traj_spins.push_back(get_spin(new_site));
     traj_sites.push_back(new_site);
     action_list.push_back(dir);
     // no repeated trajectory
@@ -180,6 +333,10 @@ int SQIceGame::go(DIR dir) {
     // agent map
     agent_map[old_site] = 0;
     agent_map[new_site] = 1; 
+
+    if (sites_counter[agent_site] < 1) {
+        state_tp1[agent_site] *= -1;
+    }
 
     steps_counter++;
     sites_counter[agent_site]++;
@@ -193,13 +350,13 @@ DIR SQIceGame::how_to_go(int site) {
     return dir;
 }
 
-int SQIceGame::icemove() {
-    int old_site = agent_site;
-    vector<int> cands = get_neighbor_candidates();
+int SQIceGame::icemove(bool same_spin) {
+    int site = -1;
+    vector<int> cands = get_neighbor_candidates(same_spin);
     if (!cands.empty()) {
-        go(how_to_go(cands[0]));
+        site = cands[0];
     }
-    return agent_site;
+    return site;
 }
 
 vector<int> SQIceGame::get_neighbor_sites() {
@@ -234,8 +391,13 @@ vector<int> SQIceGame::get_neighbor_spins() {
     return neighbors;
 }
 
-vector<int> SQIceGame::get_neighbor_candidates() {
-    int inverse_agent_spin = -1 * get_agent_spin();
+vector<int> SQIceGame::get_neighbor_candidates(bool same_spin) {
+    int inverse_agent_spin = 0;
+    if (same_spin) {
+        inverse_agent_spin = get_agent_spin();
+    } else {
+        inverse_agent_spin = -1 * get_agent_spin();
+    }
     vector<int> nspins = get_neighbor_spins();
     vector<int> nsites = get_neighbor_sites();
     vector<int> idxes;
@@ -252,7 +414,6 @@ vector<int> SQIceGame::get_neighbor_candidates() {
     // should i avoid repeating?
     return candidates;
 }
-
 
 int SQIceGame::get_neighbor_site_by_direction(DIR dir) {
     int site = agent_site;
@@ -318,6 +479,7 @@ DIR SQIceGame::get_direction_by_sites(int site, int next_site) {
         dir = UPPER_LEFT;
     }
 
+    #ifdef DEBUG
     std::cout << "right site = " << right_site << "\n"
               << "left site = " << left_site << "\n"
               << "up site = " << up_site << "\n"
@@ -326,7 +488,41 @@ DIR SQIceGame::get_direction_by_sites(int site, int next_site) {
               << "lower right site = " << lower_right_site << "\n"
               << "lower left  site = " << lower_left_site << "\n"
               << "upper left  site = " << upper_left_site << "\n";
+    #endif
+
     return dir;
+}
+
+vector<double> SQIceGame::GetEnergyMap() {
+    for (uint i = 0; i < N; i++) {
+        double se = 0.0;
+        se = _cal_energy_of_site(state_t, i);
+        energy_map[i] = se/6.0;
+    }
+    return energy_map;
+}
+
+vector<double> SQIceGame::GetStateTMap() {
+    vector<double> map(state_t.begin(), state_t.end());
+    return map;
+}
+
+vector<double> SQIceGame::GetCanvasMap() {
+    vector<double> map(canvas_traj_map.begin(),
+                        canvas_traj_map.end());
+    return map;
+}
+
+vector<double> SQIceGame::GetDefectMap() {
+    for (uint i =0; i < N; i++) {
+        double dd = 0.0;
+        if (latt.sub[i] == 1) {
+            dd = state_t[i] + state_t[latt.NN[i][0]] + state_t[latt.NN[i][1]] + state_t[latt.NNN[i][0]];
+            dd /= 4.0;
+        }
+        defect_map[i] = dd;
+    }
+    return defect_map;
 }
 
 void SQIceGame::TEST(){
@@ -353,9 +549,10 @@ void SQIceGame::TEST(){
     std::cout << "\twhose upper left site is " << get_neighbor_site_by_direction(UPPER_LEFT) << "\n";
     std::cout << "\twhose upper next site is " << get_neighbor_site_by_direction(UPPER_NEXT) << "\n";
     std::cout << "\twhose lower next site is " << get_neighbor_site_by_direction(LOWER_NEXT) << "\n";
-
     std::cout << "/=========================================/\n";
+    long_loop_algorithm();
 
+    /*
     Draw(0);
     _print_vector(get_neighbor_candidates());
     Draw(0);
@@ -380,17 +577,20 @@ void SQIceGame::TEST(){
     }
     std::cout << "traj: ";
     _print_vector(traj_sites);
+    std::cout << "no repetition traj: ";
+    _print_vector(traj_norepeat);
 
-    std::cout << "Agent site after ice move : " << icemove() << "\n";
-    std::cout << "Agent site after ice move : " << icemove() << "\n";
-    std::cout << "Agent site after ice move : " << icemove() << "\n";
-    std::cout << "Agent site after ice move : " << icemove() << "\n";
-    std::cout << "Agent site after ice move : " << icemove() << "\n";
-
+    //std::cout << "Agent site after ice move : " << icemove() << "\n";
+    //std::cout << "Agent site after ice move : " << icemove() << "\n";
+    //std::cout << "Agent site after ice move : " << icemove() << "\n";
+    //std::cout << "Agent site after ice move : " << icemove() << "\n";
+    //std::cout << "Agent site after ice move : " << icemove() << "\n";
+    flip_along_traj(traj_norepeat);
+    */
 }
 
 // ###### private function ######
-double SQIceGame::_cal_energy_of_state(vector<int> & s) {
+double SQIceGame::_cal_energy_of_state(const vector<int> & s) {
     double eng = 0.0;
     for (uint i = 0; i < N; i++) {
         double se;
@@ -418,7 +618,31 @@ double SQIceGame::_cal_energy_of_state(vector<int> & s) {
     return eng;
 }
 
-double SQIceGame::_cal_defect_density_of_state(vector<int> & s) {
+double SQIceGame::_cal_energy_of_site(const vector<int> &s, int site) {
+    double se = 0.0;
+    int i = site;
+    if (latt.sub[i] == 1) {
+        se = s[i] * (s[latt.NN[i][0]]
+                        +s[latt.NN[i][1]]
+                        +s[latt.NN[i][2]]
+                        +s[latt.NN[i][3]]
+                        +s[latt.NNN[i][0]]
+                        +s[latt.NNN[i][2]]
+                        );
+    } else {
+        se = s[i] * (s[latt.NN[i][0]]
+                        +s[latt.NN[i][1]]
+                        +s[latt.NN[i][2]]
+                        +s[latt.NN[i][3]]
+                        +s[latt.NNN[i][1]]
+                        +s[latt.NNN[i][3]]
+                        );
+    }
+    se = J1 * se;
+    return se;
+}
+
+double SQIceGame::_cal_defect_density_of_state(const vector<int> & s) {
     double dd = 0.0;
     for (uint i = 0; i < N; ++i) {
         if (latt.sub[i] == 1) {
@@ -427,10 +651,23 @@ double SQIceGame::_cal_defect_density_of_state(vector<int> & s) {
     }
     dd /= 2.0;
     dd /= N;
-    return dd;
+    return abs(dd);
 }
 
-void SQIceGame::_print_vector(vector<int> v) {
+double SQIceGame::_cal_mean(const vector<int> &s) {
+    return 1.0* std::accumulate(s.begin(), s.end(), 0LL) / s.size();
+}
+
+double SQIceGame::_cal_stdev(const vector<int> &s) {
+    double mean = _cal_mean(s);
+    vector<double> diff(s.size());
+    std::transform(s.begin(), s.end(), diff.begin(), [mean](double x) { return x - mean; });
+    double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    double stdev = std::sqrt(sq_sum/ s.size());
+    return stdev;
+}
+
+void SQIceGame::_print_vector(const vector<int> &v) {
     std::cout << "[";
     for (const auto &i: v)
         std::cout << i << ", ";
@@ -467,7 +704,7 @@ bool SQIceGame::_is_traj_continuous() {
     return cont;
 }
 
-int SQIceGame::_cal_config_diff() {
+int SQIceGame::_cal_config_difference() {
     int diff_counter = 0;
     for (size_t i = 0 ; i < N; i++) {
         if (state_0[i] != state_t[i]) {
@@ -476,4 +713,27 @@ int SQIceGame::_cal_config_diff() {
         }
     }
     return diff_counter;
+}
+
+bool SQIceGame::_is_long_loop() {
+    bool loop_occur = false;
+    return loop_occur;
+}
+
+bool SQIceGame::_is_short_loop() {
+    bool loop_occur = false;
+    return loop_occur;
+}
+
+void SQIceGame::long_loop_algorithm() {
+    set_agent_site(100);
+    int next_site = icemove(false);
+    while (!_is_start_end_meets(next_site)) {
+        Draw(how_to_go(next_site));
+        next_site = icemove(false);
+    }
+    Draw(how_to_go(next_site));
+    _print_vector(traj_norepeat);
+    flip_along_traj(traj_sites);
+    //flip_along_traj(traj_norepeat);
 }
--- src/sqice.hpp
+++ src/sqice.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <numeric>
 #include <algorithm>
 #include <string>
 
@@ -29,6 +30,8 @@ using namespace boost::python;
 */
 // 
 
+//#define DEBUG ON
+
 enum DIR {RIGHT, DOWN, LEFT, UP, LOWER_NEXT, UPPER_NEXT, LOWER_RIGHT, LOWER_LEFT, UPPER_LEFT, UPPER_RIGHT, NOWAY};
 
 
@@ -37,28 +40,44 @@ class SQIceGame {
         // Init Constructor
         SQIceGame (INFO info);
 
-        //
-
         void InitModel();
         void SetTemperature(double T);
         void MCRun(int mcSteps);
-        void Draw(int dir_idx);
-        vector<double> Metropolis();
-        inline vector<int> GetCanvasMap() {return canvas_traj_map;};
+        vector<double> Draw(int dir_idx);
+        void IceMove(int act_idx);
 
-        // 
+        vector<double> Metropolis();
+        inline void FlipTrajectory() {flip_along_traj(traj_norepeat);};
+        int Start(int init_site);
+        void Reset();
+
+        void UpdateConfig();
+
+        inline int GetAgentSite() {return get_agent_site();};
+        vector<double> GetStateTMap();
+        vector<double> GetCanvasMap(); 
+        vector<double> GetEnergyMap();
+        vector<double> GetDefectMap();
+
+        inline int GetUpdatedCounter() {return updated_counter;};
+        inline vector<int> GetAcceptedLen() {return accepted_looplength;};
 
         void update_ice_config();
         /// this function used fliiping ice config according to states?
 
         void flip_site(int site);
         void flip_agent_site();
-        void flip_along_traj();
+        void flip_along_traj(const vector<int> &traj);
 
         // return new agent site
         int go(DIR dir);
         DIR how_to_go(int site);
-        int icemove();
+
+        // return site
+        int icemove(bool);
+
+        void update_state_to_config();
+        void restore_config_to_state();
 
         void reset_maps();
 
@@ -66,28 +85,33 @@ class SQIceGame {
         DIR get_direction_by_sites(int site, int next_site);
         vector<int> get_neighbor_sites();
         vector<int> get_neighbor_spins();
-        vector<int> get_neighbor_candidates();
+        vector<int> get_neighbor_candidates(bool same_spin);
 
         void set_agent_site(int site);
         /* get funcs */
         int  get_agent_site();
         int  get_agent_spin();
-        int get_spin(int site);
+        int  get_spin(int site);
 
         // Test function
         void TEST();
 
     private:
         // private functions
-        double _cal_energy_of_state(vector<int> &s);
-        double _cal_defect_density_of_state(vector<int> &s);
+        double _cal_energy_of_state(const vector<int> &s);
+        double _cal_energy_of_site(const vector<int> &s, int site);
+        double _cal_defect_density_of_state(const vector<int> &s);
         int _cal_config_difference();
         int inline _pdb(int site, int d, int l) {return ((site + d) % l + l) % l;};
 
-        void _print_vector(vector<int> v);
+        void long_loop_algorithm();
+
+        void _print_vector(const vector<int> &v);
+        double _cal_mean(const vector<int> &s);
+        double _cal_stdev(const vector<int> &s);
         bool _is_visited(int site);
-        bool _is_start_end_meets(int site);
         bool _is_traj_continuous();
+        bool _is_start_end_meets(int site);
         bool _is_long_loop();
         bool _is_short_loop();
 
@@ -97,6 +121,9 @@ class SQIceGame {
         Square_ice model;
         Sample ice_config;
 
+        double config_mean; 
+        double config_stdev;
+
         unsigned int L, N;
         double kT;
         double h1_t, h2_t, h3_t;
@@ -105,6 +132,7 @@ class SQIceGame {
 
         unsigned int updated_counter;
         unsigned int steps_counter;
+        vector<int> accepted_looplength;
 
         // RL intefaces
         int agent_site;
@@ -124,7 +152,8 @@ class SQIceGame {
         vector<int> agent_map;
         vector<int> canvas_traj_map;
         vector<int> canvas_spin_map;
-        vector<int> eng_map;
+        vector<double> energy_map;
+        vector<double> defect_map;
         vector<int> diff_map;
 
         // utilities
@@ -154,9 +183,21 @@ BOOST_PYTHON_MODULE(libicegame)
     class_<SQIceGame>("SQIceGame", init<INFO>())
         .def("init_model", &SQIceGame::InitModel)
         .def("set_temperature", &SQIceGame::SetTemperature)
+        .def("start", &SQIceGame::Start)
+        .def("reset", &SQIceGame::Reset)
         .def("mc_run", &SQIceGame::MCRun)
         .def("draw", &SQIceGame::Draw)
+        .def("icemove", &SQIceGame::IceMove)
+        .def("get_agent_site", &SQIceGame::GetAgentSite)
         .def("get_canvas_map", &SQIceGame::GetCanvasMap)
+        .def("get_state_t_map", &SQIceGame::GetStateTMap)
+        .def("get_energy_map", &SQIceGame::GetEnergyMap)
+        .def("get_defect_map", &SQIceGame::GetDefectMap)
+        .def("metropolis", &SQIceGame::Metropolis)
+        .def("flip_trajectory", &SQIceGame::FlipTrajectory)
+        .def("update_config", &SQIceGame::UpdateConfig)
+        .def("get_updated_counter", &SQIceGame::GetUpdatedCounter)
+        .def("get_accepted_length", &SQIceGame::GetAcceptedLen)
 
         .def("TEST", &SQIceGame::TEST)
     ;
