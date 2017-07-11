a9c78ac6260579b4e994bbeddddd39ab22ce4bee
kvzhao <kelispinor@gmail.com>
2017-07-11 17:42:25 +0800

    Change weights

--- gym-icegame/gym_icegame/envs/icegame_env.py
+++ gym-icegame/gym_icegame/envs/icegame_env.py
@@ -131,7 +131,7 @@ class IceGameEnv(core.Env):
         return self.index_mapping
 
     def _draw_rets_weighting(self, rets):
-        icemove_w = -0.004
+        icemove_w = 0.002
         energy_w = -10.0
         defect_w = -10.0
         return icemove_w * rets[0] + energy_w * rets[1] + defect_w * rets[2]
