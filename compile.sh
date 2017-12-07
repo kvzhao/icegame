a8781e6c3da39584446681a9d400a0047cecea77
Ying-Jer Kao <yjkao@phys.ntu.edu.tw>
2017-12-07 10:15:20 +0800

    Fixed bugs and make it workable on CentOS 6

--- compile.sh
+++ compile.sh
@@ -5,8 +5,15 @@ set -x
 mkdir build
 cd build
 
+ANACONDA_ROOT=/home/yjkao/anaconda2
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    cmake -DCMAKE_BUILD_TYPE=DEBUG .. && make 
+   PYTHON_VERSION=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+   PYTHON_LIBRARY=$ANACONDA_ROOT/lib/libpython${PYTHON_VERSION}.so
+   PYTHON_INCLUDE_DIR=$ANACONDA_ROOT/include/python${PYTHON_VERSION}
+   PYTHON_NUMPY_INCLUDE_DIRS=$ANACONDA_ROOT/lib/python${PYTHON_VERSION}/site-packages/numpy/core/include/
+   
+   CC=icc CXX=icpc cmake -DPYTHON_LIBRARY=$PYTHON_LIBRARY -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR -DPYTHON_NUMPY_INCLUDE_DIRS=$PYTHON_NUMPY_INCLUDE_DIRS -DCMAKE_BUILD_TYPE=DEBUG .. && make
+  
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     PYTHON_VERSION=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
     PYTHON_LIBRARY=/usr/local/Frameworks/Python.framework/Versions/$PYTHON_VERSION/lib/libpython$PYTHON_VERSION.dylib
